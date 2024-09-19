83461396a809115a5dfbe6b45db71a7f2ce5a063
sploinkster <ja462577@ucf.edu>
2024-09-19 12:45:56 -0400

    Update StartSecurity.sh

--- StartSecurity.sh
+++ StartSecurity.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-echo "Haiiii!! :333 starting security measures"
+echo "\n Haiiii!! :333 starting security measures\n"
+
+
+
+#Slide 17 from 2024-09-13 Linux Security.pdf (backups)
+
+SECRET_BACKUP_DIR="/.changebackupname"
+echo "backing up /var, /etc, /opt, and /home to hidden directory $SECRET_BACKUP_DIR"
+
+mkdir $SECRET_BACKUP_DIR
+cp -avr /var $SECRET_BACKUP_DIR
+cp -avr /etc $SECRET_BACKUP_DIR
+cp -avr /opt $SECRET_BACKUP_DIR
+cp -avr /home $SECRET_BACKUP_DIR
+chattr +i -R $SECRET_BACKUP_DIR
+
+#Slide 18 from 2024-09-13 Linux Security.pdf (change passwords, to-do*)
+
