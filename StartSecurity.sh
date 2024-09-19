d47d2cf5e044809d801df987ff45e70b716f74cb
sploinkster <ja462577@ucf.edu>
2024-09-19 12:49:55 -0400

    Update StartSecurity.sh

--- StartSecurity.sh
+++ StartSecurity.sh
@@ -10,10 +10,10 @@ SECRET_BACKUP_DIR="/.changebackupname"
 echo "backing up /var, /etc, /opt, and /home to hidden directory $SECRET_BACKUP_DIR"
 
 mkdir $SECRET_BACKUP_DIR
-cp -avr /var $SECRET_BACKUP_DIR
-cp -avr /etc $SECRET_BACKUP_DIR
-cp -avr /opt $SECRET_BACKUP_DIR
-cp -avr /home $SECRET_BACKUP_DIR
+cp -ar /var $SECRET_BACKUP_DIR
+cp -ar /etc $SECRET_BACKUP_DIR
+cp -ar /opt $SECRET_BACKUP_DIR
+cp -ar /home $SECRET_BACKUP_DIR
 chattr +i -R $SECRET_BACKUP_DIR
 
 #Slide 18 from 2024-09-13 Linux Security.pdf (change passwords, to-do*)
